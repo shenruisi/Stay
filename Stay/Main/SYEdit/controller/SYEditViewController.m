ecdde0d1e6b718179fa05af3b31e386e8c4721c2
zly <453016726@qq.com>
2021-12-06 18:47:39 +0800

    feat:新增codemirror

--- Stay/Main/SYEdit/controller/SYEditViewController.m
+++ Stay/Main/SYEdit/controller/SYEditViewController.m
@@ -6,8 +6,11 @@
 //
 
 #import "SYEditViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface SYEditViewController ()
+@interface SYEditViewController ()<WKUIDelegate,WKNavigationDelegate,WKScriptMessageHandler>
+
+@property (nonatomic, strong) WKWebView *wkwebView;
 
 @end
 
@@ -15,8 +18,65 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor whiteColor];
+    self.view.backgroundColor = RGB(242, 242, 246);
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(0.0,0.0,200,44.0)];
+    [label setBackgroundColor:[UIColor clearColor]];
+    [label setNumberOfLines:0];
+    [label setTextColor:[UIColor blackColor]];
+    [label setTextAlignment:NSTextAlignmentCenter];
+    [label setText:@"content"];
+    label.font = [UIFont boldSystemFontOfSize:17];
+    self.navigationItem.titleView = label;
+    [self createView];
     // Do any additional setup after loading the view.
+}
+
+- (void)createView{
+    [self.view addSubview:self.wkwebView];
+}
+
+- (WKWebView *)wkwebView {
+    if(_wkwebView == nil) {
+        
+        WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
+        WKPreferences *preferences = [[WKPreferences alloc] init];
+        preferences.javaScriptEnabled = true;
+        [config setPreferences:preferences];
+
+        WKUserContentController * wkUController = [[WKUserContentController alloc] init];
+
+        config.userContentController = wkUController;
+
+        
+        _wkwebView = [[WKWebView alloc] initWithFrame:CGRectMake(0.0,0.0,kScreenWidth,500) configuration:config];
+        _wkwebView.UIDelegate = self;
+        _wkwebView.navigationDelegate = self;
+        _wkwebView.allowsBackForwardNavigationGestures = YES;
+        
+        [_wkwebView.configuration.userContentController addScriptMessageHandler:self  name:@"codeMirrorDidReady"];
+  
+        NSString *htmlString = [[NSBundle mainBundle] pathForResource:@"newTab" ofType:@"html"];
+
+        NSData *data = [NSData dataWithContentsOfURL:[NSURL fileURLWithPath:htmlString]];
+        [_wkwebView loadData:data MIMEType:@"text/html" characterEncodingName:@"utf-8" baseURL:[NSBundle mainBundle].resourceURL];
+        
+
+    }
+
+    return _wkwebView;
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation {
+    
+}
+
+- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message{
+    if([message.name isEqualToString:@"codeMirrorDidReady"]){
+//        [_wkwebView evaluateJavaScript:@"SetContent(\"222ddsd\")" completionHandler:^(id _Nullable, NSError * _Nullable error) {
+//        }];
+        NSLog(@"2222");
+
+    }
 }
 
 /*
