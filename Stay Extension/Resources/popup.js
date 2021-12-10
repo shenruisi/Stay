5eee04f47d5226454d5643fb24bc13a8afbcd659
turbe <cup.crystal@ymail.com>
2021-12-10 19:33:34 +0800

    click target

--- Stay Extension/Resources/popup.js
+++ Stay Extension/Resources/popup.js
@@ -220,7 +220,7 @@ function handleScriptActive(uuid, active) {
  * @param {number} type     1:match,2:console
  **/
 function handleTabAction(target, type) {
-    if (target) {
+    if (typeof type != "undefined" && type > 0) {
         document.getElementsByClassName("active-tab")[0].classList.remove("active-tab"); // 删除之前已选中tab的样式
         target.classList.add('active-tab'); // 给当前选中tab添加样式
         if(type == 1){
