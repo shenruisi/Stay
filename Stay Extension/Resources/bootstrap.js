8a6a844d8f486bb153cda29a57b23815dfc3024c
shen yin <tigris.shin@gmail.com>
2021-12-18 13:48:03 +0800

    (fix): change userscript include/exclude with regex

--- Stay Extension/Resources/bootstrap.js
+++ Stay Extension/Resources/bootstrap.js
@@ -26,8 +26,8 @@ const $_matchesCheck = (userLibraryScript,url) => {
         if (userLibraryScript.includes.length > 0){
             matched = false;
             userLibraryScript.includes.forEach((include)=>{
-                let matchPattern = new window.MatchPattern(include);
-                if (matchPattern.doMatch(url)){
+                let matchPattern = new RegExp(include);
+                if (matchPattern.test(url)){
                     matched = true;
                 }
             });
@@ -35,8 +35,8 @@ const $_matchesCheck = (userLibraryScript,url) => {
         
         
         userLibraryScript.excludes.forEach((exclude)=>{
-            let matchPattern = new window.MatchPattern(exclude);
-            if (matchPattern.doMatch(url)){
+            let matchPattern = new RegExp(exclude);
+            if (matchPattern.test(url)){
                 matched = false;
             }
         });
