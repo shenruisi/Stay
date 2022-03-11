c11f1a9614d1a9a0ab0a9afecac8d02688ab9db2
zly <453016726@qq.com>
2022-03-11 17:35:08 +0800

    feat:新增app版本

--- Stay/tampermonkey/Tampermonkey.m
+++ Stay/tampermonkey/Tampermonkey.m
@@ -51,7 +51,10 @@ static Tampermonkey *kInstance = nil;
 - (void)conventScriptContent:(UserScript *)userScript{
     NSString *scriptWithoutComment = [self _removeComment:userScript.content];
     JSValue *createGMApisWithUserScript = [self.jsContext evaluateScript:@"window.createGMApisWithUserScript"];
-    JSValue *gmApisSource = [createGMApisWithUserScript callWithArguments:@[userScript.grants,userScript.uuid]];
+    NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
+    NSString *appVersion = [infoDictionary objectForKey:@"CFBundleShortVersionString"];
+
+    JSValue *gmApisSource = [createGMApisWithUserScript callWithArguments:@[userScript,userScript.uuid,appVersion]];
     scriptWithoutComment = [NSString stringWithFormat:
                             @"async function gm_init(){\n\t%@\n\t%@\n}\ngm_init().catch((e)=>browser.runtime.sendMessage({ from: 'gm-apis', operate: 'GM_error', message: e.message, uuid:'%@'}));\n"
                             ,gmApisSource,scriptWithoutComment,userScript.uuid];
