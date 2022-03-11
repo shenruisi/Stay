52d64c3044f846faf16360305cd843ff5a16c2fa
shen yin <tigris.shin@gmail.com>
2022-03-11 18:29:27 +0800

    feat: mac catalyst

---
+++ Stay-macOS Extension/SafariWebExtensionHandler.h
@@ -0,0 +1,12 @@
+//
+//  SafariWebExtensionHandler.h
+//  Stay-macOS Extension
+//
+//  Created by ris on 2022/3/11.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SafariWebExtensionHandler : NSObject <NSExtensionRequestHandling>
+
+@end
