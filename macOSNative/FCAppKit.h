e21f2dd0dd3c31488516c593a57f9e6cd1bc8927
shen yin <tigris.shin@gmail.com>
2022-06-15 20:00:54 +0800

    initial Mac

---
+++ macOSNative/FCAppKit.h
@@ -0,0 +1,16 @@
+//
+//  FCAppKit.h
+//  Stay-Mac
+//
+//  Created by ris on 2022/6/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FCAppKit : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
