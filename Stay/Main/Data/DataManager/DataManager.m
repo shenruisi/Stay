8cad2a86d4a4033a336acfac6bfbb45b266d5718
zly <453016726@qq.com>
2022-07-20 11:18:59 +0800

    feat:新增字段

--- Stay/Main/Data/DataManager/DataManager.m
+++ Stay/Main/Data/DataManager/DataManager.m
@@ -580,7 +580,7 @@
         return;
     }
     
-    NSString *sql = @"INSERT INTO user_config_script (uuid, name, namespace, author, version, desc, homepage, icon, includes,maches,excludes,runAt,grants,noFrames,content,active,requireUrls,sourcePage,updateUrl,downloadUrl,notes,resourceUrl,update_time,switch,license) VALUES (?, ?, ?, ?, ?, ?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    NSString *sql = @"INSERT INTO user_config_script (uuid, name, namespace, author, version, desc, homepage, icon, includes,maches,excludes,runAt,grants,noFrames,content,active,requireUrls,sourcePage,updateUrl,downloadUrl,notes,resourceUrl,update_time,switch,license,iCloud_identifier) VALUES (?, ?, ?, ?, ?, ?, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
     
     sqlite3_stmt *statement;
     
@@ -650,6 +650,7 @@
         sqlite3_bind_double(statement, 23, timeString.doubleValue);
         sqlite3_bind_int(statement, 24, 0);
         sqlite3_bind_text(statement, 25, [scrpitDetail.license UTF8String], -1,NULL);
+        sqlite3_bind_text(statement, 26, [scrpitDetail.iCloudIdentifier UTF8String], -1,NULL);
     }
     
     NSInteger resultCode = sqlite3_step(statement);
