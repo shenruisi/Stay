5115eced191fbce82b1bff9116e447bb9102710a
turbo <cup.crystal@ymail.com>
2022-08-07 22:37:10 +0800

    pro style optimize

--- Stay-Mac Extension/Resources/fallback.js
+++ Stay-Mac Extension/Resources/fallback.js
@@ -15,37 +15,30 @@
         }
     }
     let browserDomain = getDomain(window.location.href);
-    
-    browser.runtime.sendMessage({from: "darkmode", operate: "FETCH_DARK_STAY"}, (response) => {
-        let fetchDarkStay = response.body;
-       
-        console.log("cleanupDarkmode---2-", (startTime - new Date().getTime()), ",fetchDarkStay=",fetchDarkStay);
-        darkModeInit(fetchDarkStay);
-        
-    });
 
-    // console.log("browser.cookies---",browser)
-    // let gettingStores = browser.cookies.getAllCookieStores();
+    let darkStayAround = window.localStorage.getItem("is_stay_around");
+    if(darkStayAround && darkStayAround !== "" && darkStayAround !== "null" && darkStayAround !== "undefined" && "a" === darkStayAround){
+        darkModeInit(darkStayAround);
+        fetchDarkStay()
+    }else{
+        browser.runtime.sendMessage({from: "darkmode", operate: "FETCH_DARK_STAY"}, (response) => {
+            darkStayAround = response.body;
+            window.localStorage.setItem("is_stay_around", darkStayAround);
+            console.log("cleanupDarkmode---1-", (startTime - new Date().getTime()), ",darkStayAround=",darkStayAround);
+            darkModeInit(darkStayAround);
+        });
+    }
+   
+    async function fetchDarkStay(){
+        browser.runtime.sendMessage({ from: "darkmode", operate: "GET_STAY_AROUND" }, function (response) {
+            let isStayAround = response.body;
+            console.log("cleanupDarkmode---2-", (new Date().getTime() - startTime), ",isStayAround=",isStayAround);
+            window.localStorage.setItem("is_stay_around", isStayAround);
+        });
+    }
 
-    // console.log("gettingStores---",gettingStores)
-    // browser.runtime.sendMessage({ from: "darkmode", operate: "GET_STAY_AROUND" }, function (response) {
-    //     let isStayAround = response.body;
-    //     console.log("cleanupDarkmode---2-", (new Date().getTime() - startTime), ",isStayAround=",isStayAround);
-    //     isStayAround = "a";
-    //     darkModeInit(isStayAround)
-    // });
-    // darkModeInit()
     console.log("fallback---endTime-", new Date().getTime());
     function darkModeInit(fetchDarkStay){
-        // let darkmodeConfig = DARK_MODE_CONFIG;
-        // let darkmodeConfig = await readLocalStorage(DARK_MODE_CONFIG);
-        // (
-        //     // 系统暗黑模式，且stay dark Mode 非关闭状态且不屏蔽网站
-        //     (matchMedia("(prefers-color-scheme: dark)").matches && "off" !== darkmodeConfig.toggleStatus && !darkmodeConfig.siteListDisabled.includes(browserDomain)) ||
-        //     // 系统非暗黑模式，且stay dark Mode 开启状态且不屏蔽网站
-        //     (!matchMedia("(prefers-color-scheme: dark)").matches && "on" === darkmodeConfig.toggleStatus && !darkmodeConfig.siteListDisabled.includes(browserDomain))
-        // )
-        
         if (
             fetchDarkStay !== "" && fetchDarkStay === "a" &&
             document.documentElement instanceof HTMLHtmlElement && 
