7e4e1c8d5f2ea6d536356f740eecf262cad35cc4
shen yin <tigris.shin@gmail.com>
2022-10-09 17:36:18 +0800

    search jump

---
+++ Stay/Mac/MacSplitViewController.m
@@ -0,0 +1,21 @@
+//
+//  MacSplitViewController.m
+//  Stay-Mac
+//
+//  Created by ris on 2022/10/9.
+//
+
+#import "MacSplitViewController.h"
+
+@interface MacSplitViewController ()
+
+@end
+
+@implementation MacSplitViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
