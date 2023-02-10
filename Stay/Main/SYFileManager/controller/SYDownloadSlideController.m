d6a27a20a82bb235af7049a159511eb5a2e02b3c
Jim <user@local.mac>
2023-02-10 18:04:31 +0800

    feat: video parse

--- Stay/Main/SYFileManager/controller/SYDownloadSlideController.m
+++ Stay/Main/SYFileManager/controller/SYDownloadSlideController.m
@@ -7,6 +7,7 @@
 
 #import "SYDownloadSlideController.h"
 #import "SYDownloadModalViewController.h"
+#import "SYParseModalViewController.h"
 
 @interface SYDownloadSlideController()
 
@@ -25,13 +26,30 @@
 
 - (ModalNavigationController *)navController{
     if (nil == _navController){
-        SYDownloadModalViewController *cer = [[SYDownloadModalViewController alloc] init];
-        cer.dic = self.dic;
-        cer.nav = self.controller;
-        _navController = [[ModalNavigationController alloc] initWithRootModalViewController:cer slideController:self];
+        NSString *downloadUrl = self.dic[@"downloadUrl"];
+        if (downloadUrl.length > 0) {
+            NSURL *linkURL = [NSURL URLWithString:downloadUrl];
+            if (linkURL != nil && ([linkURL.lastPathComponent hasSuffix:@".mp4"] || [linkURL.lastPathComponent hasSuffix:@".m3u8"])) {
+                SYDownloadModalViewController *cer = [[SYDownloadModalViewController alloc] init];
+                cer.dic = self.dic;
+                cer.nav = self.controller;
+                _navController = [[ModalNavigationController alloc] initWithRootModalViewController:cer slideController:self];
+            }
+        }
+        if (_navController == nil) {
+            SYParseModalViewController *cer = [[SYParseModalViewController alloc] init];
+            cer.dic = self.dic;
+            cer.nav = self.controller;
+            _navController = [[ModalNavigationController alloc] initWithRootModalViewController:cer slideController:self];
+        }
     }
     
     return _navController;
+}
+
+- (void)setDic:(NSMutableDictionary *)dic {
+    _dic = dic;
+    _navController = nil;
 }
 
 - (BOOL)blockAction{
