077f9159cf9ee85dd106fc60e56f94f0f50a66fd
jingd <jingd@taofen8.com>
2023-02-13 00:38:52 +0800

    feat: video parse

---
+++ Stay/Main/SYFileManager/controller/SYParseDownloadModalViewController.h
@@ -0,0 +1,18 @@
+//
+//  SYParseDownloadModalViewController.h
+//  Stay
+//
+//  Created by Jin on 2023/2/10.
+//
+
+#import "ModalViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SYParseDownloadModalViewController : ModalViewController
+@property(nonatomic,strong) UINavigationController *nav;
+
+- (void)setData:(NSArray<NSDictionary *> *)data;
+@end
+
+NS_ASSUME_NONNULL_END
