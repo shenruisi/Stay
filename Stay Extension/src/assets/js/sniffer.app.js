20b15a0ba5adeca0134dc5077d87201006c8c381
turbo <cup.crystal@ymail.com>
2023-02-13 10:35:32 +0800

    小红书

--- Stay Extension/src/assets/js/sniffer.app.js
+++ Stay Extension/src/assets/js/sniffer.app.js
@@ -106,6 +106,11 @@
         }
       }
       return uuid.join('');
+    },
+    Decode64: function(str) {
+      return decodeURIComponent(window.atob(str).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
     }
     
 
