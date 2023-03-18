fa7d294ef39f0c8f48e48ed9a8242a68c80e742a
jingd <jingd@taofen8.com>
2023-03-18 21:42:20 +0800

    feat: file manager

--- Stay/Main/SYFileManager/controller/SYFIleManagerViewController.m
+++ Stay/Main/SYFileManager/controller/SYFIleManagerViewController.m
@@ -417,6 +417,7 @@ UIDocumentPickerDelegate
                 NSDictionary *dic = [[NSUserDefaults standardUserDefaults] objectForKey:@"MY_PHONE_STORAGE"];
 
                 NSURL *fileURL = [NSURL fileURLWithPath:dic[@"url"]];
+                fileURL = [NSURL URLWithString:[fileURL.absoluteString stringByReplacingOccurrencesOfString:@"file://" withString:@"shareddocuments://"]];
                 if([[UIApplication sharedApplication] canOpenURL:fileURL]) {
                     [[UIApplication sharedApplication] openURL:fileURL];
                 }
