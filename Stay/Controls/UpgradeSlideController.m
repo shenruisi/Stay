74b3f786ada3cb5eb4b9690dc5208b462fb665bb
shen yin <tigris.shin@gmail.com>
2023-04-14 11:37:38 +0800

    upgrade slide controller

--- Stay/Controls/UpgradeSlideController.m
+++ Stay/Controls/UpgradeSlideController.m
@@ -9,24 +9,24 @@
 
 @implementation UpgradeSlideController
 
-- (instancetype)initWithContentFilter:(ContentFilter *)contentFilter{
-    if (self = [super init]){
-        self.contentFilter = contentFilter;
-    }
-    
-    return self;
-}
-
-- (ModalNavigationController *)navController{
-    if (nil == _navController){
-        ContentFilterEditModalViewController *cer = [[ContentFilterEditModalViewController alloc] init];
-        cer.contentFilter = self.contentFilter;
-        _navController = [[ModalNavigationController alloc] initWithRootModalViewController:cer slideController:self];
-        _navController.slideController = self;
-    }
-    
-    return _navController;
-}
+//- (instancetype)initWithContentFilter:(ContentFilter *)contentFilter{
+//    if (self = [super init]){
+//        self.contentFilter = contentFilter;
+//    }
+//    
+//    return self;
+//}
+//
+//- (ModalNavigationController *)navController{
+//    if (nil == _navController){
+//        ContentFilterEditModalViewController *cer = [[ContentFilterEditModalViewController alloc] init];
+//        cer.contentFilter = self.contentFilter;
+//        _navController = [[ModalNavigationController alloc] initWithRootModalViewController:cer slideController:self];
+//        _navController.slideController = self;
+//    }
+//    
+//    return _navController;
+//}
 
 - (ModalNavigationController *)modalNavigationController{
     return self.navController;
