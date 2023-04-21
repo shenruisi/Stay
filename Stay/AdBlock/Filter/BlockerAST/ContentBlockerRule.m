f7943ad9f35bae5b090bd8f0486ff326d29238ab
shen yin <tigris.shin@gmail.com>
2023-04-21 14:43:53 +0800

    merge rules

--- Stay/AdBlock/Filter/BlockerAST/ContentBlockerRule.m
+++ Stay/AdBlock/Filter/BlockerAST/ContentBlockerRule.m
@@ -22,95 +22,74 @@
     
     if (self.urlFilterIsCaseSensitive != other.urlFilterIsCaseSensitive) return NO;
     
-    if (self.ifDomain.count != other.ifDomain.count) return NO;
-    [self.ifDomain sortUsingSelector:@selector(compare:)];
-    [other.ifDomain sortUsingSelector:@selector(compare:)];
-    if (![self.ifDomain isEqualToArray:other.ifDomain]) return NO;
+    if (![self.ifDomain isEqualToSet:other.ifDomain]) return NO;
     
-    if (self.unlessDomain.count != other.unlessDomain.count) return NO;
-    [self.unlessDomain sortUsingSelector:@selector(compare:)];
-    [other.unlessDomain sortUsingSelector:@selector(compare:)];
-    if (![self.unlessDomain isEqualToArray:other.unlessDomain]) return NO;
+    if (![self.unlessDomain isEqualToSet:other.unlessDomain]) return NO;
     
-    if (self.resourceType.count != other.resourceType.count) return NO;
-    [self.resourceType sortUsingSelector:@selector(compare:)];
-    [other.resourceType sortUsingSelector:@selector(compare:)];
-    if (![self.resourceType isEqualToArray:other.resourceType]) return NO;
+    if (![self.resourceType isEqualToSet:other.resourceType]) return NO;
     
-    if (self.loadType.count != other.loadType.count) return NO;
-    [self.loadType sortUsingSelector:@selector(compare:)];
-    [other.loadType sortUsingSelector:@selector(compare:)];
-    if (![self.loadType isEqualToArray:other.loadType]) return NO;
+    if (![self.loadType isEqualToSet:other.loadType]) return NO;
     
-    if (self.ifTopUrl.count != other.ifTopUrl.count) return NO;
-    [self.ifTopUrl sortUsingSelector:@selector(compare:)];
-    [other.ifTopUrl sortUsingSelector:@selector(compare:)];
-    if (![self.ifTopUrl isEqualToArray:other.ifTopUrl]) return NO;
+    if (![self.ifTopUrl isEqualToSet:other.ifTopUrl]) return NO;
     
-    if (self.unlessTopUrl.count != other.unlessTopUrl.count) return NO;
-    [self.unlessTopUrl sortUsingSelector:@selector(compare:)];
-    [other.unlessTopUrl sortUsingSelector:@selector(compare:)];
-    if (![self.unlessTopUrl isEqualToArray:other.unlessTopUrl]) return NO;
+    if (![self.unlessTopUrl isEqualToSet:other.unlessTopUrl]) return NO;
     
-    if (self.loadContext.count != other.loadContext.count) return NO;
-    [self.loadContext sortUsingSelector:@selector(compare:)];
-    [other.loadContext sortUsingSelector:@selector(compare:)];
-    if (![self.loadContext isEqualToArray:other.loadContext]) return NO;
+    if (![self.loadContext isEqualToSet:other.loadContext]) return NO;
     
     return YES;
 }
 
-- (NSMutableArray *)ifDomain{
+- (NSMutableSet *)ifDomain{
     if (nil == _ifDomain){
-        _ifDomain = [[NSMutableArray alloc] init];
+        _ifDomain = [[NSMutableSet alloc] init];
     }
     
     return _ifDomain;
 }
 
-- (NSMutableArray *)unlessDomain{
+- (NSMutableSet *)unlessDomain{
     if (nil == _unlessDomain){
-        _unlessDomain = [[NSMutableArray alloc] init];
+        _unlessDomain = [[NSMutableSet alloc] init];
     }
     
     return _unlessDomain;
 }
 
-- (NSMutableArray *)resourceType{
+- (NSMutableSet *)resourceType{
     if (nil == _resourceType){
-        _resourceType = [[NSMutableArray alloc] init];
+        _resourceType = [[NSMutableSet alloc] init];
     }
     
     return _resourceType;
 }
 
-- (NSMutableArray *)loadType{
+- (NSMutableSet *)loadType{
     if (nil == _loadType){
-        _loadType = [[NSMutableArray alloc] init];
+        _loadType = [[NSMutableSet alloc] init];
     }
     
     return _loadType;
 }
 
-- (NSMutableArray *)ifTopUrl{
+- (NSMutableSet *)ifTopUrl{
     if (nil == _ifTopUrl){
-        _ifTopUrl = [[NSMutableArray alloc] init];
+        _ifTopUrl = [[NSMutableSet alloc] init];
     }
     
     return _ifTopUrl;
 }
 
-- (NSMutableArray *)unlessTopUrl{
+- (NSMutableSet *)unlessTopUrl{
     if (nil == _unlessTopUrl){
-        _unlessTopUrl = [[NSMutableArray alloc] init];
+        _unlessTopUrl = [[NSMutableSet alloc] init];
     }
     
     return _unlessTopUrl;
 }
 
-- (NSMutableArray *)loadContext{
+- (NSMutableSet *)loadContext{
     if (nil == _loadContext){
-        _loadContext = [[NSMutableArray alloc] init];
+        _loadContext = [[NSMutableSet alloc] init];
     }
     
     return _loadContext;
@@ -120,31 +99,31 @@
     NSMutableDictionary *ret = [[NSMutableDictionary alloc] init];
     [ret setObject:self.urlFilter forKey:@"url-filter"];
     if (self.ifDomain.count > 0){
-        [ret setObject:self.ifDomain forKey:@"if-domain"];
+        [ret setObject:[self.ifDomain allObjects] forKey:@"if-domain"];
     }
     
     if (self.unlessDomain.count > 0){
-        [ret setObject:self.unlessDomain forKey:@"unless-domain"];
+        [ret setObject:[self.unlessDomain allObjects] forKey:@"unless-domain"];
     }
     
     if (self.resourceType.count > 0){
-        [ret setObject:self.resourceType forKey:@"resource-type"];
+        [ret setObject:[self.resourceType allObjects] forKey:@"resource-type"];
     }
     
     if (self.loadType.count > 0){
-        [ret setObject:self.loadType forKey:@"load-type"];
+        [ret setObject:[self.loadType allObjects] forKey:@"load-type"];
     }
     
     if (self.ifTopUrl.count > 0){
-        [ret setObject:self.ifTopUrl forKey:@"if-top-url"];
+        [ret setObject:[self.ifTopUrl allObjects] forKey:@"if-top-url"];
     }
     
     if (self.unlessTopUrl.count > 0){
-        [ret setObject:self.unlessTopUrl forKey:@"unless-top-url"];
+        [ret setObject:[self.unlessTopUrl allObjects] forKey:@"unless-top-url"];
     }
     
     if (self.loadContext.count > 0){
-        [ret setObject:self.loadContext forKey:@"load_context"];
+        [ret setObject:[self.loadContext allObjects] forKey:@"load-context"];
     }
     
     return ret;
@@ -219,47 +198,30 @@
     
     if (!mergeDomain) return NO;
     
-    if (self.trigger.ifDomain.count > 0 || other.trigger.ifDomain.count > 0){
-        [self.trigger.resourceType sortUsingSelector:@selector(compare:)];
-        [other.trigger.resourceType sortUsingSelector:@selector(compare:)];
-        if (![self.trigger.resourceType isEqualToArray:other.trigger.resourceType]) return NO;
+    if ([self.trigger.ifDomain isEqualToSet:other.trigger.ifDomain]
+        && [self.trigger.unlessDomain isEqualToSet:other.trigger.unlessDomain]){
+        self.trigger.resourceType = [NSMutableSet setWithSet:[self.trigger.resourceType setByAddingObjectsFromSet:other.trigger.resourceType]];
+        self.trigger.loadType =  [NSMutableSet setWithSet:[self.trigger.loadType setByAddingObjectsFromSet:other.trigger.loadType]];
+        self.trigger.urlFilterIsCaseSensitive = self.trigger.urlFilterIsCaseSensitive || other.trigger.urlFilterIsCaseSensitive;
+        return YES;
     }
     
-    if (self.trigger.unlessDomain.count > 0 || other.trigger.unlessDomain.count > 0){
-        [self.trigger.resourceType sortUsingSelector:@selector(compare:)];
-        [other.trigger.resourceType sortUsingSelector:@selector(compare:)];
-        if (![self.trigger.resourceType isEqualToArray:other.trigger.resourceType]) return NO;
+    //Trigger is large
+    if ((self.trigger.resourceType.count == 0 || [other.trigger.resourceType isSubsetOfSet:self.trigger.resourceType]) && (self.trigger.loadType.count == 0 || [other.trigger.resourceType isSubsetOfSet:self.trigger.resourceType])){
+        self.trigger.ifDomain =  [NSMutableSet setWithSet:[self.trigger.ifDomain setByAddingObjectsFromSet:other.trigger.ifDomain]];
+        self.trigger.unlessDomain =  [NSMutableSet setWithSet:[self.trigger.unlessDomain setByAddingObjectsFromSet:other.trigger.unlessDomain]];
+        self.trigger.urlFilterIsCaseSensitive = self.trigger.urlFilterIsCaseSensitive || other.trigger.urlFilterIsCaseSensitive;
+        return YES;
     }
     
-    self.trigger.urlFilterIsCaseSensitive = self.trigger.urlFilterIsCaseSensitive || other.trigger.urlFilterIsCaseSensitive;
-    
-    [self.trigger.ifDomain addObjectsFromArray:other.trigger.ifDomain];
-    [self.trigger.unlessDomain addObjectsFromArray:other.trigger.unlessDomain];
-    
-    if (0 == self.trigger.resourceType.count || 0 == other.trigger.resourceType.count){
-        [self.trigger.resourceType removeAllObjects];
-    }
-    else{
-        for (NSString *resourceType in other.trigger.resourceType){
-            if (![self.trigger.resourceType containsObject:resourceType]){
-                [self.trigger.resourceType addObject:resourceType];
-            }
-        }
+    if ((other.trigger.resourceType.count == 0 || [self.trigger.resourceType isSubsetOfSet:other.trigger.resourceType]) && (other.trigger.loadType.count == 0 || [self.trigger.resourceType isSubsetOfSet:other.trigger.resourceType])){
+        self.trigger.ifDomain =  [NSMutableSet setWithSet:[self.trigger.ifDomain setByAddingObjectsFromSet:other.trigger.ifDomain]];
+        self.trigger.unlessDomain =  [NSMutableSet setWithSet:[self.trigger.unlessDomain setByAddingObjectsFromSet:other.trigger.unlessDomain]];
+        self.trigger.urlFilterIsCaseSensitive = self.trigger.urlFilterIsCaseSensitive || other.trigger.urlFilterIsCaseSensitive;
+        return YES;
     }
     
-    if (0 == self.trigger.loadType.count || 0 == other.trigger.loadType.count){
-        [self.trigger.loadType removeAllObjects];
-    }
-    else{
-        for (NSString *loadType in other.trigger.loadType){
-            if (![self.trigger.loadType containsObject:loadType]){
-                [self.trigger.loadType addObject:loadType];
-            }
-        }
-    }
-    
-    return YES;
-    
+    return NO;
 }
 
 @end
